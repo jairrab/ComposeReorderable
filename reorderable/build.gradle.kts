14834a71d2808fa103a5231dda078efc844a9fb3
Jay Barria <jaybarria@gmail.com>
2022-11-16 08:07:03 +0800

    Update build.gradle

--- reorderable/build.gradle.kts
+++ reorderable/build.gradle.kts
@@ -1,83 +1,120 @@
-import org.jetbrains.compose.ComposeBuildConfig.composeVersion
-
 plugins {
-    kotlin("multiplatform")
-    id("org.jetbrains.compose")
-    id("maven-publish")
-    id("signing")
+    id("plugins.compose-library")
 }
 
-group = "org.burnoutcrew.composereorderable"
-version = "0.9.7"
-
-kotlin {
-    jvm()
-    js(IR) {
-        browser()
-        binaries.executable()
+android {
+    sourceSets.all {
+        kotlin.srcDir("src/commonMain/kotlin")
     }
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(compose.foundation)
-                implementation(compose.animation)
-                implementation("org.jetbrains.compose.ui:ui-util:${composeVersion}")
-            }
-        }
-    }
+
+    namespace = "org.burnoutcrew.reorderable"
 }
 
-val javadocJar = tasks.register("javadocJar", Jar::class.java) {
-    archiveClassifier.set("javadoc")
+dependencies {
+    composeUiUtil()
 }
 
-publishing {
-    publications {
-        repositories {
-            maven {
-                name="oss"
-                val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-                val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-                credentials {
-                    username = extra.properties.getOrDefault("ossrh.Username", "") as String
-                    password = extra.properties.getOrDefault("ossrh.Password", "") as String
-                }
-            }
-        }
-    }
-    publications {
-        withType<MavenPublication> {
-            artifact(javadocJar)
-            pom {
-                name.set("ComposeReorderable")
-                description.set("Reorderable Compose LazyList")
-                licenses {
-                    license {
-                        name.set("Apache-2.0")
-                        url.set("https://opensource.org/licenses/Apache-2.0")
-                    }
-                }
-                url.set("https://github.com/aclassen/ComposeReorderable")
-                issueManagement {
-                    system.set("Github")
-                    url.set("https://github.com/aclassen/ComposeReorderable/issues")
-                }
-                scm {
-                    connection.set("https://github.com/aclassen/ComposeReorderable.git")
-                    url.set("https://github.com/aclassen/ComposeReorderable")
-                }
-                developers {
-                    developer {
-                        name.set("Andre Claßen")
-                        email.set("andreclassen1337@gmail.com")
-                    }
-                }
-            }
-        }
-    }
-}
+//    import org.jetbrains.compose.ComposeBuildConfig.composeVersion
+//
+//    plugins {
+//        kotlin("multiplatform")
+//        id("org.jetbrains.compose")
+//        id("maven-publish")
+//        id("signing")
+//    }
+//
+//    group = "org.burnoutcrew.composereorderable"
+//    version = "0.9.6"
+//
+//    kotlin {
+//        jvm()
+//        sourceSets {
+//            val commonMain by getting {
+//                dependencies {
+//                    implementation(compose.foundation)
+//                    implementation("org.jetbrains.compose.ui:ui-util:${composeVersion}")
+//                }
+//            }
+//        }
+//    }
+//
+//    val javadocJar = tasks.register("javadocJar", Jar::class.java) {
+//        archiveClassifier.set("javadoc")
+//    }
+//
+//    publishing {
+//        publications {
+//            repositories {
+//                maven {
+//                    name = "oss"
+//                    val releasesRepoUrl =
+//                        uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+//                    val snapshotsRepoUrl =
+//                        uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+//                    url = if (version.toString()
+//                            .endsWith("SNAPSHOT")
+//                    ) snapshotsRepoUrl else releasesRepoUrl
+//                    credentials {
+//                        username = extra.properties.getOrDefault("ossrh.Username", "") as String
+//                        password = extra.properties.getOrDefault("ossrh.Password", "") as String
+//                    }
+//                }
+//            }
+//        }
+//        publications {
+//            withType<MavenPublication> {
+//                artifact(javadocJar)
+//                pom {
+//                    name.set("ComposeReorderable")
+//                    description.set("Reorderable Compose LazyList")
+//                    licenses {
+//                        license {
+//                            name.set("Apache-2.0")
+//                            url.set("https://opensource.org/licenses/Apache-2.0")
+//                        }
+//                    }
+//                    url.set("https://github.com/aclassen/ComposeReorderable")
+//                    issueManagement {
+//                        system.set("Github")
+//                        url.set("https://github.com/aclassen/ComposeReorderable/issues")
+//                    }
+//                    scm {
+//                        connection.set("https://github.com/aclassen/ComposeReorderable.git")
+//                        url.set("https://github.com/aclassen/ComposeReorderable")
+//                    }
+//                    developers {
+//                        developer {
+//                            name.set("Andre Claßen")
+//                            email.set("andreclassen1337@gmail.com")
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    signing {
+//        sign(publishing.publications)
+//    }
 
-signing {
-    sign(publishing.publications)
-}
+// Minimum required gradle setup
+//
+//    import org.jetbrains.compose.ComposeBuildConfig.composeVersion
+//
+//            plugins {
+//                kotlin("multiplatform")
+//                // id("plugins.library")
+//                id("org.jetbrains.compose")
+//            }
+//
+//    kotlin {
+//        jvm()
+//        sourceSets {
+//            val commonMain by getting {
+//                dependencies {
+//                    implementation(compose.foundation)
+//                    implementation("org.jetbrains.compose.ui:ui-util:${org.jetbrains.compose.ComposeBuildConfig.composeVersion}")
+//                }
+//            }
+//        }
+//    }
